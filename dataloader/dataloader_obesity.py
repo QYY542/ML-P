4aab66c2f96c69562e4b1e406ecb037b75ed93e1
覃英瑜 <a2460603567@163.com>
2024-03-25 21:27:30 +0800

    nothing

--- dataloader/dataloader_obesity.py
+++ dataloader/dataloader_obesity.py
@@ -6,7 +6,7 @@ import tarfile
 import urllib
 from typing import Any, Callable, List, Optional, Union, Tuple
 import pandas as pd
-from models.define_models import TexasClassifier, Net_1
+from models.define_models import  Net_1
 
 import torch
 from torch.utils.data import Dataset
