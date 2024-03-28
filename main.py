374780ac8b5a1260a990445e4ba2d6437a3a906c
覃英瑜 <a2460603567@163.com>
2024-03-29 02:36:07 +0800

    更改聚类中心点数量

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def test_kmeans(dataset_name, model_name, selected_dataset_name, mode):
     device = torch.device("cuda")
 
     # 取前三分之一样本的数据
-    n = 800
+    n = 400
 
     # 获取三类数据集 min max random
     evaluator = KmeansDataset(dataset)
