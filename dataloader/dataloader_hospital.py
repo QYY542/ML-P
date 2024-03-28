7720129c7a2f0daab94ff88afa0ab685f4982f94
覃英瑜 <a2460603567@163.com>
2024-03-28 15:53:14 +0800

    修改hospital数据集预处理

--- dataloader/dataloader_hospital.py
+++ dataloader/dataloader_hospital.py
@@ -21,36 +21,37 @@ class Hospital(Dataset):
         # 将目标标签转换为数值
         df['readmitted'] = df['readmitted'].map({'NO': 0, '>30': 1, '<30': 2})
 
-        # 处理缺失值
-        df.replace('?', 'Unknown', inplace=True)
-
-        # 特殊处理列'weight'，因为它大部分是缺失的
-        df['weight'] = 'Unknown'
-
-        # 年龄处理，用区间中点代替区间值
-        age_mapping = {f'[{10 * i}-{10 * (i + 1)})': 5 + 10 * i for i in range(10)}
+        # 处理年龄，使用区间的中点
+        age_mapping = {'[0-10)': 5, '[10-20)': 15, '[20-30)': 25, '[30-40)': 35, '[40-50)': 45,
+                       '[50-60)': 55, '[60-70)': 65, '[70-80)': 75, '[80-90)': 85, '[90-100)': 95}
         df['age'] = df['age'].map(age_mapping)
 
-        # 处理分类数据
-        categorical_columns = ['race', 'gender', 'age', 'weight', 'admission_type_id', 'discharge_disposition_id',
+        # 定义预处理步骤
+        categorical_columns = ['race', 'gender', 'weight', 'admission_type_id', 'discharge_disposition_id',
                                'admission_source_id', 'medical_specialty', 'payer_code', 'diag_1', 'diag_2', 'diag_3',
                                'max_glu_serum', 'A1Cresult', 'change', 'diabetesMed']
-        for column in categorical_columns:
-            df[column] = df[column].fillna('Unknown')  # 先填充缺失值为'Unknown'
-            lbl = LabelEncoder()
-            df[column] = lbl.fit_transform(df[column])
+
+        # 创建预处理转换器
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_columns),
+                (
+                'num', StandardScaler(), ['time_in_hospital', 'num_lab_procedures', 'num_procedures', 'num_medications',
+                                          'number_outpatient', 'number_emergency', 'number_inpatient',
+                                          'number_diagnoses'])
+            ])
 
         # 分离特征和标签
-        X = df.drop('readmitted', axis=1).values
-        target = df['readmitted'].values
+        X = df.drop('readmitted', axis=1)
+        y = df['readmitted']
 
-        # 对数值特征进行标准化
-        scaler = StandardScaler()
-        X_scaled = scaler.fit_transform(X)
+        # 应用预处理
+        X_processed = preprocessor.fit_transform(X)
 
-        # 加载数据
-        self.X = torch.tensor(X_scaled, dtype=torch.float)
-        self.target = torch.tensor(target, dtype=torch.long)
+        # 转换为PyTorch张量
+        self.X = torch.tensor(X_processed.toarray(), dtype=torch.float)
+        self.target = torch.tensor(y.values, dtype=torch.long)
+
     def __len__(self) -> int:
         return len(self.X)
 
