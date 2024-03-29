10ae2999279621d989a1af8684fc332e1aa073d4
覃英瑜 <a2460603567@163.com>
2024-03-29 15:19:07 +0800

    添加ResNet

--- models/define_models.py
+++ models/define_models.py
@@ -156,20 +156,23 @@ class Residual(nn.Module):
         super(Residual, self).__init__()
         self.conv1 = nn.Conv1d(input_channels, num_channels, kernel_size=3, padding=1, stride=strides)
         self.conv2 = nn.Conv1d(num_channels, num_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm1d(num_channels)
+        self.bn2 = nn.BatchNorm1d(num_channels)
+        self.relu = nn.ReLU(inplace=True)
+
         if use_1x1conv:
             self.conv3 = nn.Conv1d(input_channels, num_channels, kernel_size=1, stride=strides)
         else:
             self.conv3 = None
-        self.bn1 = nn.BatchNorm1d(num_channels)
-        self.bn2 = nn.BatchNorm1d(num_channels)
 
     def forward(self, X):
-        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.relu(self.bn1(self.conv1(X)))
         Y = self.bn2(self.conv2(Y))
         if self.conv3:
             X = self.conv3(X)
         Y += X
-        return F.relu(Y)
+        return self.relu(Y)
+
 
 def resnet_block(input_channels, num_channels, num_residuals, first_block=False):
     blk = []
@@ -178,33 +181,25 @@ def resnet_block(input_channels, num_channels, num_residuals, first_block=False)
             blk.append(Residual(input_channels, num_channels, use_1x1conv=True, strides=2))
         else:
             blk.append(Residual(num_channels, num_channels))
-    return blk
+    return nn.Sequential(*blk)
+
 
 class ResNetModel(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, input_size, num_classes):
         super(ResNetModel, self).__init__()
-        self.b1 = nn.Sequential(nn.Conv1d(1, 64, kernel_size=7, stride=2, padding=3),
-                                nn.BatchNorm1d(64), nn.ReLU(),
+        self.b1 = nn.Sequential(nn.Conv1d(1, 64, kernel_size=7, stride=2, padding=3), nn.BatchNorm1d(64), nn.ReLU(),
                                 nn.MaxPool1d(kernel_size=3, stride=2, padding=1))
-        self.b2 = nn.Sequential(*resnet_block(64, 64, 2, first_block=True))
-        self.b3 = nn.Sequential(*resnet_block(64, 128, 2))
-        self.b4 = nn.Sequential(*resnet_block(128, 256, 2))
-        self.b5 = nn.Sequential(*resnet_block(256, 512, 2))
-        self.res = nn.Sequential(
-            self.b1, self.b2, self.b3, self.b4, self.b5,
-            nn.AdaptiveAvgPool1d(1),
-            nn.Flatten()
-        )
-        self.fc = nn.Sequential(
-            nn.Linear(512, 512), nn.ReLU(),
-            nn.Dropout(0.5),  # 添加Dropout层
-            nn.Linear(512, 64), nn.ReLU(),
-            nn.Dropout(0.5),  # 再次添加Dropout层
-            nn.Linear(64, num_classes)
-        )
+        self.b2 = resnet_block(64, 64, 2, first_block=True)
+        self.b3 = resnet_block(64, 128, 2)
+        self.b4 = resnet_block(128, 256, 2)
+        self.b5 = resnet_block(256, 512, 2)
+        self.final_layers = nn.Sequential(nn.AdaptiveAvgPool1d(1), nn.Flatten(), nn.Linear(512, 512), nn.ReLU(),
+                                          nn.Dropout(0.5), nn.Linear(512, 64), nn.ReLU(), nn.Dropout(0.5),
+                                          nn.Linear(64, num_classes))
 
     def forward(self, x):
-        x = self.res(x)
-        x = self.fc(x)
+        for block in [self.b1, self.b2, self.b3, self.b4, self.b5]:
+            x = block(x)
+        x = self.final_layers(x)
         return x
 
