1d03b4afb46c7d0271e399a441c2443916431f41
覃英瑜 <a2460603567@163.com>
2024-04-07 22:44:52 +0800

    测试target_train_min = min_dataset

--- main.py
+++ main.py
@@ -93,15 +93,18 @@ def test_kmeans(dataset_name, model_name, mode, train_target, train_shadow, devi
     num_features = next(iter(dataset))[0].shape[0]
     each_length = n // 2
 
-    target_train_min, target_test_min = torch.utils.data.random_split(
-        min_dataset, [each_length, each_length]
-    )
-    target_train_max, target_test_max = torch.utils.data.random_split(
-        max_dataset, [each_length, each_length]
-    )
-    target_train_random, target_test_random = torch.utils.data.random_split(
-        random_dataset, [each_length, each_length]
-    )
+    # target_train_min, target_test_min = torch.utils.data.random_split(
+    #     min_dataset, [each_length, each_length]
+    # )
+    # target_train_max, target_test_max = torch.utils.data.random_split(
+    #     max_dataset, [each_length, each_length]
+    # )
+    # target_train_random, target_test_random = torch.utils.data.random_split(
+    #     random_dataset, [each_length, each_length]
+    # )
+    target_train_min, target_test_min = min_dataset, random_dataset
+    target_train_max, target_test_max = max_dataset, random_dataset
+    target_train_random, target_test_random = random_dataset, random_dataset
 
     shadow_train, shadow_test = torch.utils.data.random_split(
         random_dataset_shadow, [each_length, each_length]
@@ -135,8 +138,8 @@ def test_kmeans(dataset_name, model_name, mode, train_target, train_shadow, devi
                            num_features)
 
     # 生成测试数据集+训练攻击模型
-    attack_trainloader, attack_min_testloader, attack_max_testloader, attack_random_testloader = get_attack_dataset_with_shadow_kmeans(target_train_min, target_test_min, target_train_max, target_test_max,
-                                          target_train_random, target_test_random, shadow_train, shadow_test,batch_size=64)
+    attack_trainloader, attack_min_testloader, attack_max_testloader, attack_random_testloader = get_attack_dataset_with_shadow_kmeans(
+        target_train_min, target_test_min, target_train_max, target_test_max,target_train_random, target_test_random, shadow_train, shadow_test,batch_size=64)
 
     test_kmeans_mia(TARGET_PATH, device, num_classes, attack_trainloader,
              attack_min_testloader,
