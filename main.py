fe76adba9ea6959d97b8c2e468bc0752395c6016
覃英瑜 <a2460603567@163.com>
2024-04-11 12:09:52 +0800

    删除了kmeans

--- main.py
+++ main.py
@@ -91,6 +91,8 @@ def test_hdbscan(dataset_name, model_name, mode, train_target, train_shadow, dev
     # 取前三分之一样本的数据
     # 这个数据和train_target_model中的batch_size有关
     n = dataset_len // 8
+    if n >= 2000:
+        n = 2000
 
     # 获取三类数据集 min max random
     evaluator = HDBSCANDataset(dataset)
