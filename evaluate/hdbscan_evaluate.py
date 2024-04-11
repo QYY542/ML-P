93ccbbbe95d8550da2e3db79f41a0b9b77e78432
覃英瑜 <a2460603567@163.com>
2024-04-11 20:56:15 +0800

    修改为余弦距离

--- evaluate/hdbscan_evaluate.py
+++ evaluate/hdbscan_evaluate.py
@@ -30,7 +30,7 @@ class HDBSCANDataset:
         print("min_cluster_size = ", self.min_cluster_size)
 
         # 使用HDBSCAN计算余弦距离聚类
-        clusterer = HDBSCAN(min_cluster_size=self.min_cluster_size, gen_min_span_tree=True)
+        clusterer = HDBSCAN(min_cluster_size=self.min_cluster_size)
         clusterer.fit(X_scaled)
         return clusterer.labels_, X_scaled, clusterer.probabilities_
 
