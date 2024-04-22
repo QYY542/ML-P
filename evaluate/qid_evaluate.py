8972b47b9875e552475ae69cfc02824b712735de
覃英瑜 <a2460603567@163.com>
2024-04-22 21:53:17 +0800

    添加差分隐私保护的数据集

--- evaluate/qid_evaluate.py
+++ evaluate/qid_evaluate.py
@@ -29,35 +29,38 @@ class QID_VE:
 
     def train_model(self, X, y, n_estimators=50):
         # 训练随机森林模型
-        model = RandomForestClassifier(n_estimators=n_estimators, random_state=42, oob_score=True)
+        model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
         model.fit(X, y)
         return model
 
+    def get_test_data(self):
+        # 获取测试集数据
+        test_loader = DataLoader(self.test_dataset, batch_size=len(self.test_dataset), shuffle=False)
+        X_test, y_test = next(iter(test_loader))
+        return X_test.numpy(), y_test.numpy()
+
     def compute_qid_impacts(self, qid_index, num_trials=30):
-        # 获取训练数据
+        # 获取训练和测试数据
         train_loader = DataLoader(self.train_dataset, batch_size=len(self.train_dataset), shuffle=False)
         X_train, y_train = next(iter(train_loader))
         X_train = X_train.numpy()
         y_train = y_train.numpy()
+        X_test, y_test = self.get_test_data()
 
-        # 训练模型并开启OOB评分
+        # 训练模型
         model = self.train_model(X_train, y_train)
 
-        # 使用OOB样本进行Permutation Importance计算
-        oob_permutation_scores = []
+        # 使用测试集进行Permutation Importance计算
+        permutation_scores = []
         for _ in range(num_trials):
-            X_train_shuffled = X_train.copy()
-            np.random.shuffle(X_train_shuffled[:, qid_index])  # 打乱特定特征
+            X_test_shuffled = X_test.copy()
+            np.random.shuffle(X_test_shuffled[:, qid_index])  # 打乱特定特征
 
-            # 检查哪些样本是OOB样本（oob_decision_function_ 中有NaN的是OOB样本）
-            oob_mask = np.isnan(model.oob_decision_function_[:, 1]) == False
-
-            # 原始OOB得分
-            oob_score_original = accuracy_score(y_train[oob_mask], model.predict(X_train)[oob_mask])
-            oob_score_shuffled = accuracy_score(y_train[oob_mask], model.predict(X_train_shuffled)[oob_mask])
-            impact = oob_score_original - oob_score_shuffled
-            oob_permutation_scores.append(impact)
+            score_original = accuracy_score(y_test, model.predict(X_test))
+            score_shuffled = accuracy_score(y_test, model.predict(X_test_shuffled))
+            impact = score_original - score_shuffled
+            permutation_scores.append(impact)
 
         # 返回平均影响值
-        return np.mean(oob_permutation_scores)
+        return np.mean(permutation_scores)
 
